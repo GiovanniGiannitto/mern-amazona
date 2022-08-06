ef81e8a80e83bf21a0c79fe144d52faca44d9266
Giovanni Giannitto <giovannigiannitto98@gmail.com>
2022-08-06 20:37:00 +0200

    Video-08-Fetch-Products

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ app.get("/api/products", (req, res, next) => {
   res.send(data.products);
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
