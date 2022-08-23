5d68a6aeb52bf5f76fb71d57a405c7b46ceefade
Giovanni Giannitto <giovannigiannitto98@gmail.com>
2022-08-23 13:29:04 +0200

    Video-16-Create-Cart-Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Badge from "react-bootstrap/esm/Badge";
 import Nav from "react-bootstrap/esm/Nav";
 import { useContext } from "react";
 import { Store } from "./Store";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   const { state } = useContext(Store);
@@ -40,6 +41,7 @@ function App() {
           <Container className="mt-3">
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/cart" element={<CartScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
