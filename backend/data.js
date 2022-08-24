8ac610f6edd16a044e21fb4550c668427e3be79d
Giovanni Giannitto <giovannigiannitto98@gmail.com>
2022-08-24 17:30:13 +0200

    Video-21-Create-Sample-Users

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+  users: [
+    {
+      name: "Giovanni",
+      email: "admin@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: true,
+    },
+    {
+      name: "Roberta",
+      email: "user@example.com",
+      password: bcrypt.hashSync("654321"),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       //_id: "1",
