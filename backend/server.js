445bb21568c2addcc4aaf873c6f71c226702ede6
Giovanni Giannitto <giovannigiannitto98@gmail.com>
2022-08-24 15:15:01 +0200

    Video-19-Connect-to-MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
 import express from "express";
 import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connect to database");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
 // test
